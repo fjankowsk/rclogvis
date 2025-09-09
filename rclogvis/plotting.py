5958cba7473bba618b6679c8dd7787b4c1ce7c9a
Fabian Jankowski <fjankowsk@gmail.com>
2025-09-09 12:50:37 +0200

    Show home point, add arrows to show the travel direction every couple of seconds, set the visible area. We should set the arrow interval based on the sampling time of the data every X seconds, or based on the travelled distance, every X meters.

--- rclogvis/plotting.py
+++ rclogvis/plotting.py
@@ -5,6 +5,7 @@
 
 import matplotlib
 from matplotlib.collections import LineCollection
+from matplotlib.patches import FancyArrowPatch
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -50,15 +51,10 @@ def plot_time_series(df, fields, title=""):
 
 
 def plot_gps_trajectory(df):
-    fig = plt.figure()
-    ax = fig.add_subplot()
+    x = (df["longitude"] - df["longitude"].iat[0]).values
+    y = (df["latitude"] - df["latitude"].iat[0]).values
 
-    points = np.array(
-        [
-            (df["longitude"] - df["longitude"].iat[0]).values,
-            (df["latitude"] - df["latitude"].iat[0]).values,
-        ]
-    ).T.reshape(-1, 1, 2)
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
     norm = plt.Normalize(df["Alt(m)"].min(), df["Alt(m)"].max())
@@ -66,12 +62,57 @@ def plot_gps_trajectory(df):
     lc = LineCollection(segments, cmap="viridis", norm=norm)
     lc.set_array(df["Alt(m)"].astype(float).values)
     lc.set_linewidth(2)
+    lc.set_zorder(5)
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
     ax.add_collection(lc)
+
+    for i in range(0, len(x) - 1, 7):
+        arrow = FancyArrowPatch(
+            (x[i], y[i]),
+            (x[i + 1], y[i + 1]),
+            mutation_scale=20,
+            arrowstyle="->",
+            color="tab:blue",
+            zorder=4,
+        )
+        ax.add_patch(arrow)
+
+    # home point
+    ax.scatter(
+        0,
+        0,
+        s=260,
+        marker="o",
+        facecolor="none",
+        edgecolor="tab:red",
+        zorder=2,
+    )
+    ax.text(
+        0,
+        0,
+        "H",
+        color="tab:red",
+        verticalalignment="center",
+        horizontalalignment="center",
+        zorder=3,
+    )
 
     plt.colorbar(lc, ax=ax, label="Altitude (m)")
 
-    ax.autoscale()
     ax.set_xlabel("Longitude (deg)")
     ax.set_ylabel("Latitude (deg)")
+
+    # set visible area
+    thresh = 0.002
+    xmin = np.minimum(-thresh, 1.2 * x.min())
+    xmax = np.maximum(thresh, 1.2 * x.max())
+    ymin = np.minimum(-thresh, 1.2 * y.min())
+    ymax = np.maximum(thresh, 1.2 * y.max())
+
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
 
     fig.tight_layout()
