f90d0caa19cac7cde8e99cdf1ec4215b11a9db3a
Fabian Jankowski <fjankowsk@gmail.com>
2025-10-05 17:01:07 +0200

    Move most of the configuration parameters from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os.path
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def get_version():
@@ -14,40 +14,6 @@ def get_version():
     return items["__version__"]
 
 
-def get_long_description():
-    with open("README.md", "r") as fd:
-        long_description = fd.read()
-
-    return long_description
-
-
 setup(
-    name="rclogvis",
     version=get_version(),
-    author="Fabian Jankowski",
-    author_email="fjankowsk@gmail.com",
-    description="Log visualisation for EdgeTX radios.",
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/fjankowsk/rclogvis",
-    license="MIT",
-    packages=find_packages(),
-    install_requires=[
-        "gpxpy",
-        "matplotlib",
-        "numpy",
-        "pandas",
-    ],
-    entry_points={
-        "console_scripts": [
-            "rclogvis-plot = rclogvis.apps.plot_data:main",
-        ],
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    zip_safe=True,
 )
